0fef2cd28d1f1213b52f4e1c5f44b9351eab634b
lucasespina <lucaseqc@al.insper.edu.br>
2025-02-19 17:03:35 +0000

    Ex1 teste

--- exe1/main.c
+++ exe1/main.c
@@ -1,27 +1,42 @@
 #include "hardware/gpio.h"
 #include "pico/stdlib.h"
 #include <stdio.h>
+#include <stdbool.h>
 
 const int BTN_PIN_R = 28;
 
+volatile bool fall_flag = false;
+volatile bool rise_flag = false;
+
+
 void btn_callback(uint gpio, uint32_t events) {
-  if (events == 0x4) { // fall edge
-    printf("fall \n");
-  } else if (events == 0x8) { // rise edge
-    printf("rise \n");
-  }
+    if (events & GPIO_IRQ_EDGE_FALL) { 
+        fall_flag = true;
+    }
+    if (events & GPIO_IRQ_EDGE_RISE) { 
+        rise_flag = true;
+    }
 }
 
 int main() {
-  stdio_init_all();
+    stdio_init_all();
 
-  gpio_init(BTN_PIN_R);
-  gpio_set_dir(BTN_PIN_R, GPIO_IN);
-  gpio_pull_up(BTN_PIN_R);
+    gpio_init(BTN_PIN_R);
+    gpio_set_dir(BTN_PIN_R, GPIO_IN);
+    gpio_pull_up(BTN_PIN_R);
 
-  gpio_set_irq_enabled_with_callback(
-      BTN_PIN_R, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, true, &btn_callback);
+    gpio_set_irq_enabled_with_callback(
+        BTN_PIN_R, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, true, &btn_callback);
 
-  while (true) {
-  }
+    while (true) {
+        if (fall_flag) {
+            printf("fall \n");
+            fall_flag = false;
+        }
+        if (rise_flag) {
+            printf("rise \n");
+            rise_flag = false;
+        }
+    }
+    return 0;
 }
