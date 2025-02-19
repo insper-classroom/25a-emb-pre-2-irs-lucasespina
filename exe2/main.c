c8e361c8f79ee87796292794b4b0c87847c10559
lucasespina <lucaseqc@al.insper.edu.br>
2025-02-19 17:11:22 +0000

    Ex2 Teste

--- exe2/main.c
+++ exe2/main.c
@@ -2,9 +2,36 @@
 #include "pico/stdlib.h"
 #include <stdio.h>
 
-int main() {
-  stdio_init_all();
+const uint LED_PIN = 4;
+const uint BTN_PIN = 28;
 
-  while (true) {
-  }
+volatile bool button_pressed_flag = false;
+
+void button_callback(uint gpio, uint32_t events) {
+    if (events & GPIO_IRQ_EDGE_FALL) {
+        button_pressed_flag = true;
+    }
+}
+
+int main() {
+    stdio_init_all();
+
+    gpio_init(LED_PIN);
+    gpio_set_dir(LED_PIN, GPIO_OUT);
+
+    gpio_init(BTN_PIN);
+    gpio_set_dir(BTN_PIN, GPIO_IN);
+    gpio_pull_up(BTN_PIN);
+
+    gpio_set_irq_enabled_with_callback(BTN_PIN, GPIO_IRQ_EDGE_FALL, true, button_callback);
+
+    while (true) {
+        if (button_pressed_flag) {
+            bool led_state = gpio_get(LED_PIN);
+            gpio_put(LED_PIN, !led_state);
+            button_pressed_flag = false;
+        }
+    }
+    
+    return 0;
 }
