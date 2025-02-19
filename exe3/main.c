522bec38fb4868e6bb02837ccc4570f653857165
lucasespina <lucaseqc@al.insper.edu.br>
2025-02-19 17:16:38 +0000

    Teste ex3

--- exe3/main.c
+++ exe3/main.c
@@ -5,12 +5,15 @@
 const int BTN_PIN_R = 28;
 const int BTN_PIN_G = 26;
 
+volatile int flag_red = 0;
+volatile int flag_green = 0;
+
 void btn_callback(uint gpio, uint32_t events) {
-  if (events == 0x4) { // fall edge
+  if (events == 0x4) {
     if (gpio == BTN_PIN_R)
-      printf("fall red\n");
+      flag_red = 1;
     else if (gpio == BTN_PIN_G)
-      printf("fall green\n");
+      flag_green = 1;
   }
 }
 
@@ -25,13 +28,18 @@ int main() {
   gpio_set_dir(BTN_PIN_G, GPIO_IN);
   gpio_pull_up(BTN_PIN_G);
 
-  // callback led r (first)
-  gpio_set_irq_enabled_with_callback(BTN_PIN_R, GPIO_IRQ_EDGE_FALL, true,
-                                     &btn_callback);
-
-  // callback led g (nao usar _with_callback)
+  gpio_set_irq_enabled_with_callback(BTN_PIN_R, GPIO_IRQ_EDGE_FALL, true, &btn_callback);
   gpio_set_irq_enabled(BTN_PIN_G, GPIO_IRQ_EDGE_FALL, true);
 
   while (true) {
+    if (flag_red) {
+      printf("fall red\n");
+      flag_red = 0;
+    }
+    if (flag_green) {
+      printf("fall green\n");
+      flag_green = 0;
+    }
   }
+  return 0;
 }
